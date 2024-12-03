b5a811b693e36c36e42e0e122fc14b4413cd2d48
Abhishek <Abhishekraut8697@gmail.com>
2024-12-03 23:55:37 +0530

    created auth middlewares

---
+++ middlewares/auth.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+// Middleware to verify JWT token
+
+export default function (req, res, next) {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: "No token, authorization denied" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Token is invalid, authorization denied" });
+  }
+}
